0bc70590052461268f349a10d5cd2029e2daa701
Guillermo Esteves <g@gesteves.com>
2024-05-18 15:27:06 -0600

    Make playlist age configurable

--- app/workers/cleanup_old_playlists_worker.rb
+++ app/workers/cleanup_old_playlists_worker.rb
@@ -2,9 +2,17 @@ class CleanupOldPlaylistsWorker < ApplicationWorker
   queue_as :low
 
   def perform
-    # We only keep playlist for a certain period.
+    # We only keep playlists for a certain period.
     # We use them to exclude songs that have already been used from the ChatGPT prompt,
     # but it's fine to reuse songs after a while, so we can delete older playlists.
-    Playlist.where('created_at < ?', 1.month.ago).destroy_all
+
+    # Default playlist age to 7 days if the environment variable is not set
+    playlist_age_days = ENV.fetch('MAX_PLAYLIST_AGE_DAYS', 7).to_i
+
+    # Calculate the cutoff date
+    cutoff_date = playlist_age_days.days.ago
+
+    # Delete playlists older than the cutoff date
+    Playlist.where('created_at < ?', cutoff_date).destroy_all
   end
 end
