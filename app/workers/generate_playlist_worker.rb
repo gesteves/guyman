d5643a37db71ab3d86f694a9c10a1a86152ae806
Guillermo Esteves <g@gesteves.com>
2024-05-18 23:00:36 -0600

    Update playlist name

--- app/workers/generate_playlist_worker.rb
+++ app/workers/generate_playlist_worker.rb
@@ -15,7 +15,7 @@ class GeneratePlaylistWorker < ApplicationWorker
     dalle_prompt = response['cover_prompt']
     playlist_tracks = response['tracks']
     playlist_description = response['description']
-    playlist_name = workout_name
+    playlist_name = "Today’s #{workout_type} workout: #{workout_name}"
 
     if playlist_id.present?
       # Update the existing playlist
