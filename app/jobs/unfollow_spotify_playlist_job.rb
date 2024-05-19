6470b132988d6aea34c1b29f3948b8adff5157bd
Guillermo Esteves <g@gesteves.com>
2024-05-19 09:57:00 -0600

    Don't remove spotify_playlist_id

--- app/jobs/unfollow_spotify_playlist_job.rb
+++ app/jobs/unfollow_spotify_playlist_job.rb
@@ -6,6 +6,5 @@ class UnfollowSpotifyPlaylistJob < ApplicationJob
     user = User.find(user_id)
     spotify_client = SpotifyClient.new(user.authentications.find_by(provider: 'spotify').refresh_token)
     spotify_client.unfollow_playlist(spotify_playlist_id)
-    user.playlists.where(spotify_playlist_id: spotify_playlist_id).update_all(spotify_playlist_id: nil)
   end
 end
