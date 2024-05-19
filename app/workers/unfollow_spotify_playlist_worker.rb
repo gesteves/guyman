46a3a63cef70935c684c6108d814baf2a839f30b
Guillermo Esteves <g@gesteves.com>
2024-05-18 21:12:22 -0600

    Tweak callback

--- app/workers/unfollow_spotify_playlist_worker.rb
+++ app/workers/unfollow_spotify_playlist_worker.rb
@@ -1,7 +1,8 @@
 class UnfollowSpotifyPlaylistWorker < ApplicationWorker
   queue_as :low
 
-  def perform(user_id, spotify_playlist_id)
+  def perform(user_id, spotify_playlist_id = nil)
+    return if spotify_playlist_id.blank?
     user = User.find(user_id)
     spotify_client = SpotifyClient.new(user.authentications.find_by(provider: 'spotify').refresh_token)
     spotify_client.unfollow_playlist(spotify_playlist_id)
