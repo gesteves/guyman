b38d061be8360793b6f75ef453fd97b4fbb7ece2
Guillermo Esteves <g@gesteves.com>
2024-05-19 09:06:05 -0600

    Only allow certain emails

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,13 +5,21 @@ module Users
     end
 
     def handle_auth(kind)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+      auth = request.env["omniauth.auth"]
+      allowed_email = ENV['ALLOWED_AUTH_EMAIL']
+
+      unless auth.info.email == allowed_email
+        render plain: "Forbidden", status: :forbidden
+        return
+      end
+
+      @user = User.from_omniauth(auth)
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
       else
-        session["devise.#{kind.downcase}_data"] = request.env["omniauth.auth"]
+        session["devise.#{kind.downcase}_data"] = auth
         redirect_to new_user_registration_url
       end
     end
