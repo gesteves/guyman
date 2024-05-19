c178655c84d8aa2767e1976aeb6083c762c6381d
Guillermo Esteves <g@gesteves.com>
2024-05-18 21:24:18 -0600

    Add comments

--- app/workers/clean_up_spotify_playlists_worker.rb
+++ app/workers/clean_up_spotify_playlists_worker.rb
@@ -10,14 +10,22 @@ class CleanUpSpotifyPlaylistsWorker < ApplicationWorker
       todays_workouts = TrainerroadClient.new(preference.calendar_url, preference.timezone).get_workouts_for_today
       workout_names = todays_workouts.map { |workout| workout[:name] }
 
-      # Unfollow any Spotify playlists created before the current day
+      # Unfollow any Spotify playlists created before the current day.
+      # These playlists were likely used in a workout, so we want to unfollow them
+      # to avoid cluttering the user's Spotify account, but we don't want to delete them
+      # from the database so we don't reuse their songs in future playlists.
       user.playlists.where('created_at < ?', current_date.beginning_of_day).find_each do |playlist|
         if playlist.spotify_playlist_id.present?
           UnfollowSpotifyPlaylistWorker.perform_async(user.id, playlist.spotify_playlist_id)
         end
       end
 
-      # Unfollow and delete from the database any Spotify playlists created today that don't match today's workouts
+      # Unfollow and delete from the database any Spotify playlists created today that don't match today's workouts.
+      # These playlists probably reference a workout that was previously scheduled for today and has since been removed
+      # from the calendar, probably because the user either deleted it, rescheduled it, or replaced it with an alternate.
+      # In this case we DO want to delete the playlist from the database so we can reuse its songs in future playlists,
+      # since playlist was likely never used.
+      # Note that destroying the playlist will also unfollow it in Spotify.
       user.playlists.where(created_at: current_date.beginning_of_day..current_date.end_of_day).find_each do |playlist|
         unless workout_names.include?(playlist.workout_name)
           playlist.destroy
