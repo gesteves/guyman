b2ad9e458c7518d71c20aace23609c84728ccc8d
Guillermo Esteves <g@gesteves.com>
2024-05-19 08:16:55 -0600

    Add a validation

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -4,4 +4,5 @@ class Preference < ApplicationRecord
   validates :musical_tastes, presence: true
   validates :calendar_url, presence: true
   validates :timezone, presence: true
+  validates_format_of :calendar_url, with: /\A(https?:\/\/)?(api\.)?trainerroad\.com\/.+\z/i, message: "must be a valid TrainerRoad calendar URL"
 end
