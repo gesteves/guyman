436ba7d7abc5dc22127d180d969616f10660a68d
Guillermo Esteves <g@gesteves.com>
2024-05-18 19:44:39 -0600

    Use the correct date

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   end
 
   def playlist_for_workout(workout_name)
-    current_date = Time.current.in_time_zone(preference.timezone).to_date
+    current_date = Time.current.in_time_zone(preference.timezone)
     playlists.where(workout_name: workout_name)
              .where(created_at: current_date.beginning_of_day..current_date.end_of_day)
              .first
