3765d6bd7321c59f81937ebd36b21fd49edcedf1
Guillermo Esteves <g@gesteves.com>
2024-05-20 06:32:13 -0600

    Add safe navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @preference = current_user&.preference
-    @todays_playlists = current_user.todays_playlists
+    @todays_playlists = current_user&.todays_playlists
   end
 end
