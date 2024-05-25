1e2c3ebec09fd46dd2c111adad60cbffc585b857
Guillermo Esteves <g@gesteves.com>
2024-05-25 07:56:35 -0600

    Empty existing controller tests

--- test/controllers/playlists_controller_test.rb
+++ test/controllers/playlists_controller_test.rb
@@ -1,7 +1,5 @@
 require "test_helper"
 
 class PlaylistsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
--- test/controllers/preferences_controller_test.rb
+++ test/controllers/preferences_controller_test.rb
@@ -1,23 +1,5 @@
 require "test_helper"
 
 class PreferencesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get preferences_new_url
-    assert_response :success
-  end
 
-  test "should get create" do
-    get preferences_create_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get preferences_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get preferences_update_url
-    assert_response :success
-  end
 end
