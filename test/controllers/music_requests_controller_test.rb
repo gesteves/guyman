d35c4830753f0e9b20b732172667edb19fd6bee2
Guillermo Esteves <g@gesteves.com>
2024-05-26 15:47:55 -0600

    Remove unused tests

--- test/controllers/music_requests_controller_test.rb
+++ test/controllers/music_requests_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class MusicRequestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get music_requests_index_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get music_requests_update_url
-    assert_response :success
-  end
 end
