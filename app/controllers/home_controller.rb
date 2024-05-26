76fb2b05de9f37143ae50c529c4e75a29f4381d9
Guillermo Esteves <g@gesteves.com>
2024-05-26 15:48:07 -0600

    Remove instance variable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @preference = current_user&.preference || current_user.build_preference
     @todays_playlists = current_user&.todays_playlists
     @music_request = current_user&.current_music_request || current_user.music_requests.build
     @page_title = "Today’s Playlists"
