086ae0f5839fd0adc861d27c0c3d0c84da217fdc
Guillermo Esteves <g@gesteves.com>
2024-05-26 07:07:02 -0600

    Add new view for login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @preference = current_user&.preference
     @todays_playlists = current_user&.todays_playlists
