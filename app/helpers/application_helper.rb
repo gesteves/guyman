d05fc70985ade9bd05116657332d535e2babab4c
Guillermo Esteves <g@gesteves.com>
2024-05-26 12:04:24 -0600

    Add notification colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def notification_class(key)
+    case key
+    when "notice"
+      "is-success"
+    when "alert"
+      "is-warning"
+    else
+      "is-info"
+    end
+  end
 end
