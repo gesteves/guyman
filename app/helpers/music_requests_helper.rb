43d356053ecbcd2caebdfa3f091c2168c2417b22
Guillermo Esteves <g@gesteves.com>
2024-05-27 08:08:40 -0600

    Tweak actions

--- app/helpers/music_requests_helper.rb
+++ app/helpers/music_requests_helper.rb
@@ -1,2 +1,14 @@
 module MusicRequestsHelper
+
+  def music_request_delete_confirmation_message(request)
+    if request.active?
+      'Deleting this request will regenerate today’s playlists using the next one. Are you sure you want to continue? This can’t be undone.'
+    else
+      'Are you sure you want to delete this request? This can’t be undone.'
+    end
+  end
+
+  def music_request_buttons_disabled
+    current_user.todays_playlists.any?(&:processing?)
+  end
 end
