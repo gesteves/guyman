4452d87a10d1a96a9a1e77b49943b5ff5225e4eb
Guillermo Esteves <g@gesteves.com>
2024-05-28 08:10:47 -0600

    Hardcode song count

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -57,14 +57,6 @@ class Playlist < ApplicationRecord
     !locked?
   end
 
-  # Returns the minimum number of songs required for the playlist based on the workout duration,
-  # assuming 2 minutes per song, but ensuring it returns a minimum of 10 songs and a maximum of 200 songs.
-  #
-  # @return [Integer] The minimum number of songs.
-  def minimum_song_count
-    (workout_duration / 2.0).ceil.clamp(10, 200)
-  end
-
   private
 
   def unfollow_spotify_playlist
