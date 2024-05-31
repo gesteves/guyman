a6a7dc01221318ec0efd32232f379fe91bd827b3
Guillermo Esteves <g@gesteves.com>
2024-05-31 08:52:04 -0600

    Handle exceptions

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,11 @@
 class ApplicationJob
   include Sidekiq::Job
   sidekiq_options queue: 'default'
+
+  sidekiq_retry_in do |count, exception, jobhash|
+    case exception
+    when FrozenError, ActiveRecord::RecordNotFound
+      :kill
+    end
+  end
 end
