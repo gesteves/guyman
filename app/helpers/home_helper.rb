5ef5d40e421b13e9889dd635fabcea6b6a3344d8
Guillermo Esteves <g@gesteves.com>
2024-05-31 07:08:50 -0600

    Remove confirmation

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,21 +1,4 @@
 module HomeHelper
-  def lock_button_confirmation_message(playlist)
-    automatically_clean_up_old_playlists = playlist.user.preference&.automatically_clean_up_old_playlists
-    if automatically_clean_up_old_playlists
-      if playlist.locked?
-        "Unlocking the playlist will allow it to be regenerated and be automatically deleted tomorrow. Are you sure you want to unlock it?"
-      else
-        "Locking the playlist will prevent it from being regenerated or being automatically deleted tomorrow. Are you sure you want to lock it?"
-      end
-    else
-      if playlist.locked?
-        "Unlocking the playlist will allow it to be regenerated again. Are you sure you want to unlock it?"
-      else
-        "Locking the playlist will prevent it from being regenerated. Are you sure you want to lock it?"
-      end
-    end
-  end
-
   def generate_playlists_button_class(playlists)
     css_class = ["button is-fullwidth"]
     css_class << "is-loading" if playlists.present? && playlists.any?(&:processing)
