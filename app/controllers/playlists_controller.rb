8743833196e4f048e6499a1a61aa364d0b1dcd69
Guillermo Esteves <g@gesteves.com>
2024-06-01 21:21:26 -0600

    Fix action

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,6 @@
 class PlaylistsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_playlist, only: [:toggle_lock, :regenerate, :regenerate_cover, :follow, :unfollow]
+  before_action :set_playlist, only: [:toggle_lock, :regenerate, :regenerate_cover, :toggle_follow]
 
   def index
     page = params[:page]&.to_i || 1
