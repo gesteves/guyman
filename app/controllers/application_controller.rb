96ee04cdb75ca5f29c4a33ad7c7cd3bb267f06f2
Guillermo Esteves <g@gesteves.com>
2024-06-02 07:48:56 -0600

    Use flash for notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :turbo_stream_flash
 
-  def turbo_stream_notification(options)
-    turbo_stream.update("notification", partial: "shared/flash", locals: options)
+  def turbo_stream_notification
+    return if flash.blank?
+    turbo_stream.update("notification", partial: "shared/flash")
   end
 end
