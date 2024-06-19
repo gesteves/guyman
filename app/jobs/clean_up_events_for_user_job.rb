58add57ee591ea886a9c971dc8c28f5e7facee43
Guillermo Esteves <g@gesteves.com>
2024-06-19 09:46:11 -0600

    Move event cleanup to User

--- app/jobs/clean_up_events_for_user_job.rb
+++ app/jobs/clean_up_events_for_user_job.rb
@@ -3,22 +3,6 @@ class CleanUpEventsForUserJob < ApplicationJob
 
   def perform(user_id)
     user = User.find(user_id)
-    preference = user.preference
-
-    current_date = Time.current.in_time_zone(preference.timezone)
-    event_names = user.todays_calendar_events.map { |event| event[:name] }
-
-    # Unfollow and delete from the database any events created today that don't match today's events in the calendar,
-    # and don't have any locked playlists
-    # These events probably reference a workout that was previously scheduled for today and has since been removed
-    # from the calendar, probably because the user either deleted it, rescheduled it, replaced it with an alternate,
-    # or TrainerRoad adapted the plan.
-    user.activities.joins(:playlist)
-        .where(created_at: current_date.beginning_of_day..current_date.end_of_day)
-        .where(playlists: { locked: false })
-        .where.not(name: event_names)
-        .each do |activity|
-      activity.destroy
-    end
+    user.destroy_activities_removed_from_calendar
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -131,4 +131,21 @@ class User < ApplicationRecord
       UnfollowSpotifyPlaylistJob.perform_async(id, playlist.spotify_playlist_id)
     end
   end
+
+  # Deletes from the database any activities created today that don't match today's events in the calendar, and don't have any locked playlists.
+  # These events probably reference a workout that was previously scheduled for today and has since been removed
+  # from the calendar, probably because the user either deleted it, rescheduled it, replaced it with an alternate,
+  # or TrainerRoad adapted the plan.
+  def destroy_activities_removed_from_calendar
+    current_date = Time.current.in_time_zone(preference.timezone)
+    event_names = todays_calendar_events.map { |event| event[:name] }
+
+    activities.joins(:playlist)
+        .where(created_at: current_date.beginning_of_day..current_date.end_of_day)
+        .where(playlists: { locked: false })
+        .where.not(name: event_names)
+        .each do |activity|
+      activity.destroy
+    end
+  end
 end
