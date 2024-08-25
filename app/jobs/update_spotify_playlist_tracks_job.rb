5f2274adffe0939d1bd3adf229bb66f64096b2b5
Guillermo Esteves <g@gesteves.com>
2024-08-25 03:39:22 -0700

    Limit retries

--- app/jobs/update_spotify_playlist_tracks_job.rb
+++ app/jobs/update_spotify_playlist_tracks_job.rb
@@ -1,5 +1,6 @@
 class UpdateSpotifyPlaylistTracksJob < ApplicationJob
   queue_as :high
+  sidekiq_options retry_for: 1.hour
 
   def perform(user_id, playlist_id)
     user = User.find(user_id)
@@ -10,5 +11,11 @@ class UpdateSpotifyPlaylistTracksJob < ApplicationJob
     spotify_client.update_playlist_tracks(playlist.spotify_playlist_id, playlist.spotify_uris)
 
     playlist.done_processing!
+  end
+
+  sidekiq_retries_exhausted do |msg, exception|
+    playlist_id = msg['args'][1]
+    playlist = Playlist.find(playlist_id)
+    playlist.activity.destroy if playlist.present?
   end
 end
