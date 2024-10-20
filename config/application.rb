6f645315c2bd9cb9230c4d7076c53c07d1f5de75
Guillermo Esteves <g@gesteves.com>
2024-10-20 09:50:36 -0600

    Upgrade Rails

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative "boot"
+
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,12 +9,12 @@ Bundler.require(*Rails.groups)
 module WorkoutPlaylistGenerator
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
