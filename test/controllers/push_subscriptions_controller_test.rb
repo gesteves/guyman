05b96a95475ceab0617b97121f51b0e68bf1170c
Guillermo Esteves <g@gesteves.com>
2024-10-26 12:12:11 -0600

    Fix tests

--- test/controllers/push_subscriptions_controller_test.rb
+++ test/controllers/push_subscriptions_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class PushSubscriptionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get push_subscriptions_create_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get push_subscriptions_destroy_url
-    assert_response :success
-  end
 end
