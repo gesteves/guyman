0c2aa088d72b3db1fcd63c3920b625e56f1ea501
Guillermo Esteves <g@gesteves.com>
2024-10-26 16:53:50 -0600

    Remember me

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,7 +16,7 @@ module Users
       @user = User.from_omniauth(auth)
 
       if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
+        sign_in_and_redirect @user, event: :authentication, remember_me: true
         set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
       else
         session["devise.#{kind.downcase}_data"] = auth
